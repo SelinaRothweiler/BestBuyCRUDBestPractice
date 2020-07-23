bbde48b3eeb4e31bec772cecca26e51e7b23f573
Sel007 <selina.08225.1@gmail.com>
2020-07-23 11:48:45 -0400

    Add project files.

---
+++ BestBuyCrudBestPracticesConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuyCrudBestPracticesConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
